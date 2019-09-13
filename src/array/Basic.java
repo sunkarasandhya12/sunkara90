a885b19181cf41d140ce9c10c5d9f730c843ba77
sunkarasandhya12 <sunkara.sandyarani@gmail.com>
2019-09-13 23:17:33 +0530

    git program

---
+++ src/array/Basic.java
@@ -0,0 +1,18 @@
+package array;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		int num[]=new int[5];
+		num[0]=10;
+		num[1]=20;
+		num[3]=30;
+		num[4]=40;
+		 num[3]=30;
+		for(int i=0;i<num.length;i++)
+		System.out.println(num[i]);
+				
+
+	}
+
+}
